32708ac85570e220251300f69365da394ba55d33
Ephantus karanja <ephantusslogan@gmail.com>
2018-05-11 14:54:45 +0300

    add compute current total for player1

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var randomNumber;
 var randomNumber2;
+var totalscore1=0;
+var totalscore2=0;
 //Business Logic
 function roll1() {
     randomNumber = Math.floor(Math.random() * 6) + 1;
@@ -18,14 +20,20 @@ $(document).ready(function (event) {
     $("#roll1").click(function () {
         roll1();
         //alert(randomNumber)
-        $("#dicescore1").append(randomNumber);
+        $(".dicescore1").text(randomNumber);
+        totalscore1=totalscore1 + randomNumber;
+        //alert(totalscore1);
+        $(".currentscore1").text(totalscore1);
+
     });
     $("#rolldice2").click(function (){
         roll2();
         //alert(randomNumber2)
-        $("#dicescore2").append(randomNumber2);
+        $(".dicescore2").text(randomNumber2);
     });
     //print dice roll to dice score
+
+
 
 });
 
