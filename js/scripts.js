4cc72261c7bf04d6dad0dae129004a94f902d43f
Ephantus karanja <ephantusslogan@gmail.com>
2018-05-11 16:21:52 +0300

    add rule 2 exception function

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ var randomNumber;
 var randomNumber2;
 var totalscore1=0;
 var totalscore2=0;
+// var winnerscore=
 //Business Logic
 function roll1() {
     randomNumber = Math.floor(Math.random() * 6) + 1;
@@ -11,7 +12,24 @@ function roll2() {
     randomNumber2 = Math.floor(Math.random() * 6) + 1;
     return randomNumber2;
 };
-
+function testdice(){
+    if (randomNumber >1){
+        totalscore1 +=randomNumber;
+        
+    }
+    else{
+        totalscore1=0;  
+    }
+};
+function testdice2(){
+    if (randomNumber2 >1){
+        totalscore2 +=randomNumber2;
+        
+    }
+    else{
+        totalscore2=0;  
+    }
+};
 
 
 
@@ -19,21 +37,18 @@ function roll2() {
 $(document).ready(function (event) {
     $("#roll1").click(function () {
         roll1();
-        //alert(randomNumber)
         $(".dicescore1").text(randomNumber);
-        totalscore1 +=randomNumber;
-        //alert(totalscore1);
+        testdice();
         $(".currentscore1").text(totalscore1);
 
     });
     $("#rolldice2").click(function (){
         roll2();
-        //alert(randomNumber2)
         $(".dicescore2").text(randomNumber2);
-        totalscore2 +=randomNumber2;
+        testdice2();
         $(".currentscore2").text(totalscore2);
     });
-    //print dice roll to dice score
+    
 
 
 
