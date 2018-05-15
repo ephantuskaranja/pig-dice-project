cf5a87f1af1c52b59ec0eea11b257abc0526e7a8
Ephantus karanja <ephantusslogan@gmail.com>
2018-05-15 13:37:43 +0300

    add winner determinant function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 var randomNumber;
 var randomNumber2;
-var totalscore1=0;
-var totalscore2=0;
-// var winnerscore=
+var totalscore1 = 0;
+var totalscore2 = 0;
+var inputtedwinnerscore;
 //Business Logic
 function roll1() {
     randomNumber = Math.floor(Math.random() * 6) + 1;
@@ -12,56 +12,70 @@ function roll2() {
     randomNumber2 = Math.floor(Math.random() * 6) + 1;
     return randomNumber2;
 };
-function testdice(){
-    if (randomNumber >1){
-        totalscore1 +=randomNumber;
-        
+function testdice() {
+    if (randomNumber > 1) {
+        totalscore1 += randomNumber;
+
     }
-    else{
-        totalscore1=0; 
-        alert("You Rolled a 1!");   
+    else {
+        totalscore1 = 0;
+        alert("You Rolled a 1!");
     }
 };
-function testdice2(){
-    if (randomNumber2 >1){
-        totalscore2 +=randomNumber2;
-        
+function testdice2() {
+    if (randomNumber2 > 1) {
+        totalscore2 += randomNumber2;
+
     }
-    else{
-        totalscore2=0;
-        alert("You Rolled a 1!");  
+    else {
+        totalscore2 = 0;
+        alert("You Rolled a 1!");
+    }
+};
+function winnerTester() {
+    if (inputtedwinnerscore <= totalscore1) {
+        alert("Player1 wins");
+    }
+    else if (inputtedwinnerscore <= totalscore2) {
+        alert("Player2 wins");
     }
 };
 
 
 //user interface
 $(document).ready(function (event) {
-    $("#submit").click(function(){ 
+    $("#submit").click(function () {
         var inputtedPlayer1Name = $("input#player1name").val();
         var inputtedPlayer2Name = $("input#player2name").val();
         $(".player1").append("player1:" + inputtedPlayer1Name);
-        $(".player2").append("player2:"+ inputtedPlayer2Name);
+        $(".player2").append("player2:" + inputtedPlayer2Name);
         $(".rowplayer").hide();
 
     });
+    $("#setscore").click(function () {
+        inputtedwinnerscore = $("input#winner").val();
+        //alert(inputtedwinnerscore);
+    });
+
     $("#roll1").click(function () {
         roll1();
         $(".dicescore1").text(randomNumber);
         testdice();
         $(".currentscore1").text(totalscore1);
+        winnerTester();
 
     });
-    $("#rolldice2").click(function (){
+    $("#rolldice2").click(function () {
         roll2();
         $(".dicescore2").text(randomNumber2);
         testdice2();
         $(".currentscore2").text(totalscore2);
+        winnerTester();
     });
-    $("#newgame").click(function(){
+    $("#newgame").click(function () {
         location.reload();
-
     });
-    
+
 });
 
 
